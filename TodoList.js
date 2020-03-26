799e99aa05b37c342c78c87749fb9cbb627976e4
leticelvin <54833746+leticelvin@users.noreply.github.com>
2020-03-26 20:47:10 +0100

    Added button and a little styling

--- TodoList.js
+++ TodoList.js
@@ -6,10 +6,15 @@ import TodoItem from "./TodoItem";
 export default class TodoList extends Component {
   render() {
      return (
-      <div>
-       <h1>TodoList</h1>
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">
+        Todo list
+        </h3>
         <TodoItem />
-     </div>
+        <button type="button" className="btn btn-danger btn-block text-capitalize mt-5">
+        Clear list
+        </button>
+      </ul>
      );
   }
 }
