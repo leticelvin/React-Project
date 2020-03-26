fc71141e9863483b1b63a9905d413444cf60e2e0
leticelvin <54833746+leticelvin@users.noreply.github.com>
2020-03-26 19:53:12 +0100

    Added input field

--- Todoinput.js
+++ Todoinput.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 
+//Todoinput.js
+
 export default class TodoInput extends Component {
   render() {
-    return {
-      <div>
-      </div> 
-      }
-   }
+    return (
+      <div className="card card-body my-3">
+        <form>
+         <div className="input-group">
+          <div className="input-group-prepend">
+            <div className="input-group-text bg-primary text-white">
+	          <i className="fas fa-book" />
+	           </div>
+	        </div>
+	        <input type="text" classname="form-control text-capitalize" placeholder="Add task"/>
+	      </div>
+       </form>
+      </div>
+      );
+     }
 }
