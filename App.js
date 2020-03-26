d6e692ac8bba5e4607b3d3909c3494cc638ef6bd
leticelvin <54833746+leticelvin@users.noreply.github.com>
2020-03-26 14:16:36 +0100

    Added styling

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import TodoInput from "./TodoInput";
-import TodoItem from "./TodoItem";
+import TodoInput from "./components/TodoInput";
+import TodoItem from "./components/TodoItem";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
