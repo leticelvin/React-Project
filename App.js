7204a35e6e7682ba183e259f6db5de88f3d014f1
leticelvin <54833746+leticelvin@users.noreply.github.com>
2020-03-27 21:33:35 +0100

    Added more functions

--- App.js
+++ App.js
@@ -41,8 +41,12 @@ editItem: false
                  <div className="row">
                   <div className="col-10 mx-auto col-md-8 mt-4">
                    <h2> className="text-capitalize text-center">Input</h2>
-                    <TodoInput item={this.state.item} handleChange={this.handleChange} />
-                    <TodoList />
+                    <TodoInput 
+		     item={this.state.item} 
+		     handleChange={this.handleChange}
+		     handleSubmit={this.handleSubmit}
+		     />
+                    <TodoList items={this.state.items} />
                   </div>
                  </div>
                 </div>
