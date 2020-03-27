1409b43fc5a2ff7e3e4a06b54ccc08e01487410e
leticelvin <54833746+leticelvin@users.noreply.github.com>
2020-03-27 19:54:12 +0100

    Added props mechanism

--- App.js
+++ App.js
@@ -4,13 +4,23 @@ import TodoItem from "./components/TodoItem";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
+state={
+  items: [],
+  id:0,
+  item: ' ',
+  editItem: false
+};
+
+handleChange = (e) => {
+  this.setState({ item: e.target.value });
+};
     render() {
              return (
                 <div className="container">
                  <div className="row">
                   <div className="col-10 mx-auto col-md-8 mt-4">
                    <h2> className="text-capitalize text-center">Input</h2>
-                    <TodoInput />
+                    <TodoInput item={this.state.item} handleChange={this.handleChange} />
                     <TodoList />
                   </div>
                  </div>
