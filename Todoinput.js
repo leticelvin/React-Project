600b824f5cf343016d62bbb024da6e6a36edea60
leticelvin <54833746+leticelvin@users.noreply.github.com>
2020-03-27 19:57:19 +0100

    props

--- Todoinput.js
+++ Todoinput.js
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 
 export default class TodoInput extends Component {
   render() {
+   const {item, handleChange } = this.props
     return (
       <div className="card card-body my-3">
         <form>
